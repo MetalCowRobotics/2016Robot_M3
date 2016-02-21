7995d1b6091718aefd69d8397e3e7b0e49ec782a
Will S <willofmetal85@gmail.com>
2016-02-21 15:56:54 -0600

    IntakeMap is done

---
+++ src/org/usfirst/frc/team4213/robot/systems/IntakeMap.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team4213.robot.systems;
+
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Talon;
+
+public class IntakeMap {
+
+public static final SpeedController INTAKE_MOTOR = new Talon(RobotMap.Intake.MOTOR_CHANNEL);
+
+}
