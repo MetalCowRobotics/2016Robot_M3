f35723af962a7a47d573af11a987ef79035596f4
Will S <willofmetal85@gmail.com>
2016-02-21 17:23:04 -0600

    TurretPitchMap is done.

---
+++ src/org/usfirst/frc/team4213/robot/systems/TurretPitchMap.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team4213.robot.systems;
+
+import edu.wpi.first.wpilibj.CANTalon;
+
+import org.usfirst.frc.team4213.robot.systems.RobotMap.Turret.Pitch_Motor;
+
+
+public class TurretPitchMap {
+public static final CANTalon PITCH_MOTOR = new CANTalon(Pitch_Motor.MOTOR_CHANNEL);
+public static double getEncoder(){
+	return PITCH_MOTOR.getEncPosition();
+}
+}
