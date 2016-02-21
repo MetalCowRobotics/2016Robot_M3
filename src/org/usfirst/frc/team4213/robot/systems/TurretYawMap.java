044881de9d919d9d75bbfecd29b8a9132e672e9a
Will S <willofmetal85@gmail.com>
2016-02-21 17:31:24 -0600

    YawTurretMap is done

---
+++ src/org/usfirst/frc/team4213/robot/systems/TurretYawMap.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team4213.robot.systems;
+import org.usfirst.frc.team4213.robot.systems.RobotMap.Turret.Yaw_Motor;
+
+import edu.wpi.first.wpilibj.CANTalon;
+
+public class TurretYawMap {
+
+	public static final CANTalon YAW_MOTOR = new CANTalon(Yaw_Motor.MOTOR_CHANNEL);
+	
+	//TODO add string potentiometer
+	
+}
