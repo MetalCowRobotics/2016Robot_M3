5f2c3ecc6dc042eeb2cb1f64575fe7e0d92fc5fa
Will S <willofmetal85@gmail.com>
2016-02-22 17:09:25 -0600

    Zeros to string pot

--- src/org/usfirst/frc/team4213/robot/controllers/TurretYawController.java
+++ src/org/usfirst/frc/team4213/robot/controllers/TurretYawController.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team4213.robot.controllers;
 
+import org.usfirst.frc.team4213.robot.Robot;
 import org.usfirst.frc.team4213.robot.systems.TurretYawMap;
 public class TurretYawController {
 	TurretYawMap turretYaw; 
@@ -31,5 +32,16 @@ public class TurretYawController {
 		if(turretYaw.LOWER_LIMIT <= turretYaw.CURRENT_POSITION){
 			turretYaw.CLOCK_WISE_SPEED = 0;
 		}
+	}
+	
+	public void zeroYaw(){
+		double error = turretYaw.START_POSITION - turretYaw.CURRENT_POSITION;
+		if(turretYaw.CURRENT_POSITION < 500){
+			turretYaw.COUNTER_CLOCK_WISE_SPEED = Math.abs(error/100);
+		}
+		else{
+			turretYaw.CLOCK_WISE_SPEED = Math.abs(error/100);
+		}
+
 	}
 }
