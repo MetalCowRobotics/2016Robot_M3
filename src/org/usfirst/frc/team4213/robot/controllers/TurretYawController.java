09c1cc760e6abffad4f8c65efa39c28b24ba0072
Will S <willofmetal85@gmail.com>
2016-02-21 18:15:14 -0600

    not done yet.

---
+++ src/org/usfirst/frc/team4213/robot/controllers/TurretYawController.java
@@ -0,0 +1,24 @@
+package org.usfirst.frc.team4213.robot.controllers;
+
+import org.usfirst.frc.team4213.robot.systems.TurretYawMap;
+public class TurretYawController {
+	TurretYawMap turretYaw; 
+	
+	
+	public TurretYawController(TurretYawMap turretYawMap){
+		turretYaw = turretYawMap;
+	}
+	
+	private void turretYawMove(double speed){
+		turretYaw.YAW_MOTOR.set(speed);
+	}
+	
+	private void yawClockWise(){
+		turretYawMove(turretYaw.CLOCK_WISE_SPEED);
+	}
+	
+	public void yawCounterClockWise(){
+		spinIntake(turretYaw.COUNTER_CLOCK_WISE_SPEED);
+	}
+	
+}
