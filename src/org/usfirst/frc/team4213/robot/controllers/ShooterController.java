5dae51b0726ff884ca8a39ab29c52a43ba3ca6ae
Krithik Rao <krit2000@live.com>
2016-02-22 16:43:10 -0600

    Added Proper State Handling
    I added lots of state handling and setup all the remaining functions for
    the shooter. Next task : setup test periodic to do the right thing.

--- src/org/usfirst/frc/team4213/robot/controllers/ShooterController.java
+++ src/org/usfirst/frc/team4213/robot/controllers/ShooterController.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team4213.robot.controllers;
 
-import org.team4213.lib14.Xbox360Controller;
+import java.util.Calendar;
+
 import org.usfirst.frc.team4213.robot.systems.RobotMap.Shooter;
 import org.usfirst.frc.team4213.robot.systems.ShooterMap;
 
@@ -8,15 +9,16 @@ public class ShooterController {
 	private ShooterMap shooter;
 	private int desiredCamAngle;
 	public ShooterState state;
-	private boolean intakeReady;
+	private long armTime;
+	private long shootTime;
 
 	public ShooterController(ShooterMap shooter) {
 		this.shooter = shooter;
 		state = ShooterState.IDLE;
-		intakeReady = true;
+		//shooter.camMotor.enableBrakeMode(true); // DOES THIS WORK ???? 
+		desiredCamAngle = 0;
 		shooter.resetEnc();
 		shooter.camEncoder.setDistancePerPulse(1 / Shooter.COUNT_PER_DEG);
-
 	}
 
 	public enum ShooterState {
@@ -24,23 +26,60 @@ public class ShooterController {
 	}
 
 	public void arm() {
+		shooter.setWheelSpeed(Shooter.SHOOT_SPEED);
+		armTime = getCurrentTimeMS();
 		state = ShooterState.ARMING;
 	}
 
 	public void intake() {
+		shooter.setWheelSpeed(Shooter.INTAKE_SPEED);
 		state = ShooterState.INTAKE;
 	}
 
 	public void shoot() {
+		desiredCamAngle = 360;
+		shootTime = getCurrentTimeMS();
 		state = ShooterState.SHOOTING;
 	}
-
-	public void run(Xbox360Controller controller, int tState) {
-		// TODO IMPLEMENT STATE HANDLING AND CONTROLLER INPUT
+	public void idle(){
+		shooter.setCamSpeed(0);
+		shooter.setWheelSpeed(0);
+		state = ShooterState.IDLE;
 	}
-
-	public void isolatedRun(Xbox360Controller controller) {
-		// TODO IMPLEMENT STATE HANDLING AND CONTROLLER INPUT
+	
+	public void lowerCam(){
+		desiredCamAngle = 120; // ADD TO CONFIG
+		
 	}
-
+	
+	private long getCurrentTimeMS(){
+		return  Calendar.getInstance().get(Calendar.MILLISECOND);
+	}
+	
+	private void runCamPID(){
+		double error = desiredCamAngle - shooter.getEncDist();
+		shooter.setCamSpeed(error / 180);
+	}
+	
+	public void step(){
+		switch(state){
+		case SHOOTING:
+			if(Math.abs(desiredCamAngle - shooter.getEncDist()) < 5 && getCurrentTimeMS() - shootTime >  1*1000 ){ // 1 Second of Wheel Spinning. ( ADD TO CONFIG )
+				shooter.resetEnc();
+				idle();
+				desiredCamAngle = 0;
+			}
+			break;
+		case ARMING:
+			if(getCurrentTimeMS()-armTime > 2*1000){ // 2 Seconds, Can be changed ( ADD TO CONFIG )
+				state = ShooterState.ARMED;		
+			}
+			break;
+		default:
+			break;
+		}
+		
+		runCamPID();
+	}
+	
 }
