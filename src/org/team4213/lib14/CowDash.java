1df85dd8a2aa80325f4d84320aeddfcec71db9a6
Thaddeus Hughes <Thaddeus Hughes>
2016-03-02 15:41:40 -0600

    Implemented saving to disk in CowDash. Have not tested. org.json library added.

--- src/org/team4213/lib14/CowDash.java
+++ src/org/team4213/lib14/CowDash.java
@@ -1,9 +1,74 @@
 package org.team4213.lib14;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class CowDash {
 	// TODO: Save to disk and read from disk; see 2015
+	
+	private static final String savefileName = "./dashboardvalues";
+	
+	public static void save() {
+		NetworkTable table = NetworkTable.getTable("SmartDashboard");
+		
+		JSONObject jsobj = new JSONObject();
+		
+		for(String key:table.getKeys()) {
+			Object thing = table.getValue(key, null);
+			try{
+				jsobj.put(key, thing);
+			}catch(Exception e){
+				
+			}
+		}
+		
+		try (FileWriter file = new FileWriter(savefileName)) {
+			String out = jsobj.toString();
+			if (out!=null) {
+				file.write(out);
+				DriverStation.reportError("\nCowDash.save: Success!\n", false);
+			} else {
+				DriverStation.reportError("\nCowDash.save: jsobj.toString() returns null\n", true);
+			}
+		} catch (Exception e){
+			DriverStation.reportError("\nCowDash.save: could not write to file\n", true);
+		}
+	}
+	
+	public static void load() {
+		JSONObject savefile=null;
+		try {
+			savefile = (JSONObject) new JSONObject( new JSONTokener(new FileReader(savefileName)));
+		} catch (FileNotFoundException e) {
+			DriverStation.reportError("\nCowDash.load: could not find file\n", true);
+			return;
+		} catch (JSONException je) {
+			DriverStation.reportError("\nCowDash.load: could not parse JSON\n", true);
+			return;
+		}
+		
+		for(String key:savefile.keySet()){
+			Object val = savefile.get(key);
+			if (val instanceof Double) {
+				CowDash.setNum(key, ((Double) val).doubleValue());
+			} else if (val instanceof Boolean) {
+				CowDash.setBool(key, ((Boolean) val).booleanValue());
+			} else if (val instanceof String) {
+				CowDash.setString(key, val.toString());
+			}
+		}
+		DriverStation.reportError("\nCowDash.load: Success!\n", false);
+		
+	}
 	
 	public static double getNum(String key, double fallback) {
 		try{
