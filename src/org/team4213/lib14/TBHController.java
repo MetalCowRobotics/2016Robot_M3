19a4d2aab00071925ee9f0e4bd5a992f90a0da00
Will S <willofmetal85@gmail.com>
2016-03-08 21:22:14 -0600

    Tuned Vision Tracking and Motor Speed

---
+++ src/org/team4213/lib14/TBHController.java
@@ -0,0 +1,53 @@
+package org.team4213.lib14;
+
+public class TBHController{
+	
+	private double lastPWM;
+	private double desiredRPS;
+	private double lastError;
+	private double ki;
+	private final String name;
+	
+	// Throw Half Back Controller ( Google The Name )
+	
+	public TBHController(String name){
+		this.name = name;
+		lastPWM = 0;
+	}
+	public TBHController(String name, double ki) {
+		this(name);
+		CowDash.setNum(name + "::ki", ki);
+
+	}
+	
+	public void setTarget(double rps){
+		desiredRPS = rps;
+	}
+
+	public double feedAndGetValue(double currentRPS, double currentPWM) {
+		CowDash.setNum(name+"::currentRPS", currentRPS);
+		CowDash.setNum(name+"::currentPWM", currentPWM);
+		if(desiredRPS == 0){
+			return 0;
+		}
+		
+		if(lastError == 0){
+			lastError =  desiredRPS - currentRPS;
+		}
+		final double newError = desiredRPS - currentRPS;
+		ki = CowDash.getNum(name + "::ki", 0.1)/1000;
+		final double newPWM;
+		if(newError*lastError < 0){
+			newPWM = (currentPWM+lastPWM)/2;
+		}else if(newError * lastError > 0){
+			newPWM = (lastError*ki) +currentPWM;
+		}else{
+			newPWM = currentPWM;
+		}
+		
+		lastError = newError;
+		lastPWM = currentPWM;
+		return newPWM;
+	}
+	
+}
